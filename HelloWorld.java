a166809141713caf4984c3b44339e159a875fcfd
akshitgawade <akshitpgawade@gmail.com>
2026-07-04 11:06:27 +0530

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! 🎉");
+        System.out.println("Java is working perfectly in VS Code!");
+        System.out.println("Current time: " + java.time.LocalDateTime.now());
+        
+        // Simple test
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
