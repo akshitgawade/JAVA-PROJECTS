ca5092d2bd95278128a497b7a3312826e417b761
akshitgawade <akshitpgawade@gmail.com>
2026-07-28 23:04:24 +0530

    Committing java exp 6

---
+++ LAB EXP 6/Ex2.java
@@ -0,0 +1,42 @@
+
+interface DeliveryStatus {
+    void updateStatus();
+}
+
+class FoodDelivery {
+    int orderId;
+    String customerName;
+    String foodItem;
+
+    FoodDelivery (int orderId, String customerName, String foodItem) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.foodItem = foodItem;
+    }
+
+    class OrderDetails {
+        void display() {
+            System.out.println("Order ID      : " + orderId);
+            System.out.println("Customer Name : " + customerName);
+            System.out.println("Food Item     : " + foodItem);
+        }
+    }
+}
+
+public class Ex2 {
+    public static void main(String[] args) {
+        FoodDelivery order = new FoodDelivery(1383, "Akshit", "Taco");
+
+        FoodDelivery.OrderDetails details = order.new OrderDetails();
+        details.display();
+
+        DeliveryStatus status = new DeliveryStatus() {
+            @Override
+            public void updateStatus() {
+                System.out.println("Delivery Status : Delivered.");
+            }
+        };
+
+        status.updateStatus();
+    }
+}
