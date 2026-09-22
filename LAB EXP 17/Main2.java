f046b7095f0648304b25b50b5fd7c45e76241acd
akshitgawade <akshitpgawade@gmail.com>
2026-09-22 15:43:47 +0530

    committing 17-20 - No pdfs

---
+++ LAB EXP 17/Main2.java
@@ -0,0 +1,8 @@
+import java.applet.*;
+import java.awt.*;
+
+public class Main2 extends Applet {
+    public void paint (Graphics g) {
+        g.drawString("Welcome to Java Applet", 50, 50);        
+    }
+}
